41587d1c19a331714ede2c51dcbe3e7655eecdb1
haroldwongms <stuart.kirk@microsoft.com>
2020-03-30 00:42:48 -0400

    Updated content for RHEL8

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -59,7 +59,9 @@ echo "**************************************************************************
         runuser -l student -c "pip-2.7 install --upgrade --user pyOpenSSL"
         runuser -l student -c "pip-2.7 install --user ansible==2.9.6"
         yum -y remove rhn-check rhn-client-tools rhn-setup rhnlib rhnsd yum-rhn-plugin PackageKit* subscription-manager >>/root/yum-output.log
-        echo "[ssh_connection]" > /home/student/.ansible.cfg
+        echo "[defaults]" > /home/student/.ansible.cfg
+        echo "no_log = True" >> /home/student/.ansible.cfg
+        echo "[ssh_connection]" >> /home/student/.ansible.cfg
         echo "ssh_args = -o StrictHostKeyChecking=no" >> /home/student/.ansible.cfg
         chown student:student /home/student/.ansible.cfg
 echo "********************************************************************************************"	
