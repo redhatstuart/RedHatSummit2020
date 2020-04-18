386cb36b2d5f4c064d2f4f19b17d96ab7a8c839d
Stuart R. Kirk <stuart.kirk@microsoft.com>
2020-04-18 01:56:20 -0400

    Updates for ARO lab

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -71,6 +71,11 @@ echo "**************************************************************************
 	sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 	yum -y install azure-cli >> /root/yum-output.log
 echo "********************************************************************************************"	
+	echo "`date` -- Installing MSSQL Tools" >>/root/provision-script-output.log
+        runuser -l student -c "echo PATH="$PATH:/opt/mssql-tools/bin" >> ~/.bash_profile"
+        runuser -l student -c "echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc"
+        ACCEPT_EULA=Y yum install -y mssql-tools unixODBC-devel
+echo "********************************************************************************************"	
 	echo "`date` -- Setting default systemd target to graphical.target" >>/root/provision-script-output.log
 	systemctl set-default graphical.target >> /root/provision-script-output.log
 echo "********************************************************************************************"
