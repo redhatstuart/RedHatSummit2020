2b1536d75631ac25013a050c894c7f30888ddffd
Stuart R. Kirk <stuart.kirk@microsoft.com>
2020-04-17 23:14:15 -0400

    Updates for ARO lab

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
 print "<h1 align=middle>Hello there!<br><br>";
-print "Welcome to Azure Red Hat OpenShift!<br><br>";
+print "Welcome to Azure Red Hat OpenShift!!<br><br>";
 print "<img align=middle src=https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/images/microsoft-twitter.png><br><br>";
 print "Your php version is " . PHP_VERSION . "\n";
 ?>
