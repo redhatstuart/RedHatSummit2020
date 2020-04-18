c9a71d828f8ce3d3e9d2fd74ffe306e56abe60ec
haroldwongms <stuart.kirk@microsoft.com>
2020-04-17 21:47:23 -0400

    Updates for ARO lab

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
-print "Hello there! Welcome to Azure Red Hat OpenShift!  Your php version is " . PHP_VERSION . "\n";
+print "<h1 align=middle>Hello there!<br><br>";
+print "Welcome to Azure Red Hat OpenShift!<br><br>";
+print "<img align=middle src=https://thedesignlove.com/wp-content/uploads/2018/02/Case-Study-The-Microsoft-Logo-Evolution-5.jpg><br><br>";
+print "Your php version is " . PHP_VERSION . "\n";
 ?>
