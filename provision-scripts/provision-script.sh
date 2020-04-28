24ef142ecfad3a981af0acd1320da03bd16790dd
Stuart R. Kirk <stuart.kirk@microsoft.com>
2020-04-28 19:35:34 -0400

    Updated provision script

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -148,7 +148,6 @@ echo "**************************************************************************
 echo "********************************************************************************************"
         chown -R student:student /home/student/.local
         chmod a+rx /home/student/.local
-#        yum -y update
         cd /usr/local/bin
         wget -P /usr/local/bin https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/oc.tar.gz
         tar xvfz oc.tar.gz
